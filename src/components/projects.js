9c57617a80bbc30477ac5f1ddf750280714a4d35
s028041 <kugeleviciusd@gmail.com>
2019-05-30 14:26:37 +0300

    Projects button is now active

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,65 +1,103 @@
-import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
-
+import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardText,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton
+} from "react-mdl";
 
 class Projects extends Component {
   constructor(props) {
     super(props);
     this.state = { activeTab: 0 };
   }
+  handleClick() {
+    console.log("Project 1 button has been clicked");
+    // window.location.href = "https://github.com/s028041/React-Router";
+    window.open("https://github.com/s028041/React-Router", "_blank");
+  }
+  handleClick2() {
+    console.log("Project 2 button has been clicked");
+    // window.location.href = "https://github.com/s028041/React-Router";
+    window.open("https://github.com/s028041/React-Router", "_blank");
+  }
 
   toggleCategories() {
-
-    if(this.state.activeTab === 0){
-      return(
+    if (this.state.activeTab === 0) {
+      return (
         <div className="projects-grid">
           {/* Project 1 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
-            <CardText>
-             Some text about project    
-        </CardText>
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover"
+              }}
+            >
+              React Project #1
+            </CardTitle>
+            <CardText>React Router libary and fetch api</CardText>
             <CardActions border>
-              <Button colored>GitHub</Button>
-              
+              <Button onClick={this.handleClick} colored>
+                GitHub
+              </Button>
             </CardActions>
-            <CardMenu style={{color: '#fff'}}>
+            <CardMenu style={{ color: "#fff" }}>
               <IconButton name="share" />
             </CardMenu>
           </Card>
           {/* Project 2 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #2</CardTitle>
-            <CardText>
-                Text about project
-            </CardText>
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover"
+              }}
+            >
+              React Project #2
+            </CardTitle>
+            <CardText>Text about project</CardText>
             <CardActions border>
-              <Button colored>GitHub</Button>
-            
+              <Button onClick={this.handleClick2} colored>
+                GitHub
+              </Button>
             </CardActions>
-            <CardMenu style={{color: '#fff'}}>
+            <CardMenu style={{ color: "#fff" }}>
               <IconButton name="share" />
             </CardMenu>
           </Card>
-          
         </div>
-      )
+      );
     }
   }
   render() {
-    return(
+    return (
       <div>
-        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
           <Tab>React</Tab>
-          
         </Tabs>
-          <Grid>
-            <Cell col={12}>
-              <div className="content">{this.toggleCategories()}</div>
-            </Cell>
-          </Grid>
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
       </div>
-    )
+    );
   }
 }
 export default Projects;
